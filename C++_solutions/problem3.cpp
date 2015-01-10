2f36339f91a14284e361b315628e4e7c6d3c71b5
RoboEvangelist <elpot_15@hotmail.com>
2015-01-10 02:22:02 -0800

    using array to store, and return factor only

--- C++_solutions/problem3.cpp
+++ C++_solutions/problem3.cpp
@@ -8,27 +8,24 @@ What is the largest prime factor of the number 600851475143 ?
 
 #include <iostream>
 
-//long divide_factor(long number_and_factor[]);
-int divide_factor(int number, int factor);
+long divide_factor(long number_and_factor[]);
 // Precondition: gets the number of interest, and the largest factor
 // Postcondition: returns the number of interes, and the new largest factor
 
 int main(){
 
-    // {number, factor}
-    long number = 600851475143;    // number to get factors from
-    int first_factor = 2;                // max initial factor
-//    long number_and_factor[] = {600851475143, 2};
-    std::cout << std::endl << "the maximum prime factor is of " << number
-              << " is: " << divide_factor(number, first_factor) 
+    // {number of interest, maximum factor}
+    long number_and_factor[] = {600851475143, 2};
+    std::cout << std::endl << "the maximum prime factor is of " 
+              << number_and_factor[0]
+              << " is: " << divide_factor(number_and_factor) 
               << std::endl << std::endl; 
 }
                 
-//long divide_factor(long number_and_factor[]){
-int divide_factor(int number, int factor){
+long divide_factor(long number_and_factor[]){
 //    if(n==1) 
 //        return(1);
 //    if(n==2) 
 //        return(2);
-    return (factor);       // For n > 2
+    return(number_and_factor[1]);       // For n > 2
 }
